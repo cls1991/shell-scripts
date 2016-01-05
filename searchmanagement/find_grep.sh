77cc820d37658d75da0a1486fd0edddd519129a3
cls1991 <tzk19910406@hotmail.com>
2016-01-05 18:05:17 +0800

    Update find_grep.sh

--- searchmanagement/find_grep.sh
+++ searchmanagement/find_grep.sh
@@ -2,3 +2,5 @@
 
 # 查找phpmyadmin目录下包括"Welcome to "内容的php文件
 find phpmyadmin -type f -regex ".*\.php" -exec grep "Welcome to " {} \; -print
+# 统计多种类型的文件的个数
+find src/ -type f -regex ".*\.h\|.*\.c" | wc -l
