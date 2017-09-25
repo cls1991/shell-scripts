5d201edfd6ca27cca3242d072383d0f027746ff8
cls1991 <tzk19910406@hotmail.com>
2017-09-25 14:54:50 +0800

    更新find脚本, 添加path exclude功能

--- searchmanagement/find_grep.sh
+++ searchmanagement/find_grep.sh
@@ -4,3 +4,8 @@
 find phpmyadmin -type f -regex ".*\.php" -exec grep "Welcome to " {} \; -print
 # 统计多种类型的文件的个数
 find src/ -type f -regex ".*\.h\|.*\.c" | wc -l
+# 排除多个无关目录
+# 第一种: -name参数匹配范围更广, 比如, './a', './other/a'等
+# 第二种: 仅匹配列举出的目录
+find . -type d \( -name a -o -name b \) -prune -o -print
+find . -path './a' -prune -o -path './b' -prune -o -print
